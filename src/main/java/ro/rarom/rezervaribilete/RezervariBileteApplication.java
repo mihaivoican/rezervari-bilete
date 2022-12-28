3a2ca08058c5d9e10cbbb1d4420ffe982821a5a2
mihaivoican <mihaivoican@yahoo.com>
2022-12-28 11:49:28 +0200

    first commit

---
+++ src/main/java/ro/rarom/rezervaribilete/RezervariBileteApplication.java
@@ -0,0 +1,13 @@
+package ro.rarom.rezervaribilete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezervariBileteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezervariBileteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/rarom/rezervaribilete/RezervariBileteApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.rarom.rezervaribilete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezervariBileteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
